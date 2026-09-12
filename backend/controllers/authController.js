4dc2d27c6e51372bc45249ab4b506a07fdb100b6
JuliaTrang <hoangtrang555htm@gmail.com>
2026-09-12 19:56:17 +0700

    :sparkle: auth logic setup

---
+++ backend/controllers/authController.js
@@ -0,0 +1,70 @@
+const bcrypt = require('bcrypt');
+const db = require('../models');
+
+const SALT = 10;
+
+const register = async(req,res)=>{
+    try{
+        const {username, email, password} = req.body;
+        if(!username || !email || !password){
+            return res.status(400).json({message: "do not leave blanks!"});
+        }
+        const user_email = db.Users.findOne({where: {email}});
+        if(user_email){
+            return res.status(401).json({message: "existing email!"});
+        }
+
+        const pass = await bcrypt.hash(password,SALT);
+        const new_user = await db.Users.create({
+            username,
+            email,
+            pass
+        });
+
+        return res.status(200).json({message: "register successfully!"});
+    }
+    catch(err){
+        return res.status(500).json({message: "error connecting!", error: err});
+    }
+}
+
+const login = async(req,res)=>{
+    try{
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({message: "do not leave blanks!"});
+        }
+        const user = db.Users.findOne({where: {email}});
+        if(!user){
+            return res.status(401).json({message: "user not exist!"});
+        }
+        const isMatch = await bcrypt.compare(password, user.pass);
+        if(!isMatch){
+            return res.status(400).json({message: "login failed!"});
+        }
+        req.session.userId = user.id;
+        return res.status(200).json({message: "login successfully!"})
+    }
+    catch(err){
+        return res.status(500).json({message:"error connecting!", error: err});
+    }
+}
+
+const logout = async(req,res)=>{
+    req.session.destroy((err)=>{
+        if(err){
+            return res.status(500).json({message: "server error!"});
+        }
+        res.clearCookie('connect.sid',{
+            httpOnly: true,
+            sameSite: 'strict'
+        });
+        return res.status(200).json({ message: 'Logout successful' });
+    })
+}
+
+module.exports = {
+    register,
+    login,
+    logout
+}
